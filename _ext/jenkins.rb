e13615efa98e18be0293a9aef07934e557ae0486
Aslak Knutsen <aslak@redhat.com>
2014-01-12 19:31:59 +0100

    Add Jenkins test result status to samples/index

--- _ext/jenkins.rb
+++ _ext/jenkins.rb
@@ -12,35 +12,54 @@ module Awestruct::Extensions::Jenkins
 
     def execute(site)
 
-      site.results = []
+      site.results = OpenStruct.new
+      site.results.extend(Locators)
+      site.results.containers = []
+      site.results.tests = {}
 
       jobs_overview_url = API_URL % [@base_url]
       jobs_overview = RestClient.get jobs_overview_url, :accept => 'application/json', :cache_expiry_age => DURATION_1_DAY
       jobs_overview['jobs'].select{|j|j['name'] =~ /.*Samples on.*/}.each do |job| 
         
-        container = OpenStruct.new
-        site.results << container
+        container = nil
         if job['name'] =~ /.*Samples on (.+)\-cb.*/
-          container.name = $1 
+          name = $1
+          container = site.results.containers.find{|c|c.name.eql? name}
+          if container.nil?
+            container = OpenStruct.new
+            container.name = $1
+            site.results.containers << container
+          end
         else 
           return #ignore unknown name pattern
         end
-
-        container.tests = []
 
         result_url = TEST_RESULTS % [job['url']]
         result = RestClient.get result_url, :accept => 'application/json', :cache_expiry_age => DURATION_1_DAY
 
         result['suites'].each do |suite|
           suite['cases'].each do |c|
-            
-            #puts "#{c['className']}-#{c['name']}"
 
-            test = OpenStruct.new
-            test.name = c['name']
-            test.status = c['status']
-            container.tests << test
+            class_name = c['className']
+            test_name = c['name']
 
+            test = site.results.tests[class_name]
+            if test.nil?
+              test = OpenStruct.new
+              test.class_name = class_name
+              test.children = {}
+              site.results.tests[class_name] = test
+            end
+
+            method = test.children[test_name]
+            if method.nil?
+              method = OpenStruct.new
+              method.name = test_name
+              method.status = {}
+              test.children[test_name] = method
+            end
+
+            method.status[container] = c['status']
           end
         end
       end
@@ -48,5 +67,31 @@ module Awestruct::Extensions::Jenkins
       #puts site.results
     end
 
+    module Locators
+
+      def summary_by_sample(sample)
+        return {} if sample.tests.nil?
+        a = sample.tests.map{
+          |test|
+          summary_by_testclass(test.qualified_name, test)
+          }.inject{|a, b| a.merge(b) {|k, x, y| x and y}}
+        return a
+      end
+
+      def summary_by_testclass(qualified_name, scenario)
+        results = self.tests[qualified_name]
+        return {} if results.nil?
+        result = {}
+        number_of_tests = scenario.children.select{|x| x.is_test}.size
+        for value in results.children.each_value
+          value.status.each{
+            |k, v|
+              result[k.name] ||= 0; result[k.name] +=1 if v.eql? "PASSED"
+          }
+        end
+        result.each{|k, v| result[k] = v == number_of_tests ? true : false}
+      end
+
+    end
   end
 end
